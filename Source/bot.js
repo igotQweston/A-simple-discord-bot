d726a6e10b14e81de80538f1288bea33e00fc49c
igotQweston <65166424+igotQweston@users.noreply.github.com>
2021-01-23 13:54:37 +0700

    Add files via upload

--- Source/bot.js
+++ Source/bot.js
@@ -2,33 +2,41 @@
 
 const {Client, MessageEmbed} = require('discord.js');
 const client = new Client();
+const token = require('./token.json')
+const id = require('./ID.json')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+var log =' '
+
 client.on('message', msg => {
-  console.log(msg.author,',',msg.content)
+  if(msg.author.id!="778133865477701654") {
+    console.log(msg.author.tag,',',msg.channel.name,',',msg.createdAt,',',msg.content)
+    console.log(msg.channel)
+    var logs = msg.author.tag.concat(' , ',msg.channel,' , ',msg.createdAt,' , ',msg.content)
+    log = new MessageEmbed()
+        .setTitle('Log')
+        .setColor(0xff0000)
+        .setDescription(logs);  
+    client.users.fetch(id).then((user) => {
+        user.send(log)
+  })}
+
   if (msg.content === 'db help') {
     const embed = new MessageEmbed()
-        // Set the title of the field
         .setTitle('Why??')
-        // Set the color of the embed
         .setColor(0xff0000)
-        // Set the main content of the embed
         .setDescription("Why are you asking help? I don't have any cool features because the guy who made me is lazy. Maybe in the future he will add a cool feature. For now just don't ask me for help.");
-    // Send the embed to the same channel as the message
     msg.channel.send(embed);
-}}
+}})
 
 ,client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
-  // Do nothing if the channel wasn't found on this server
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
   if (!channel) return;
-  // Send the message, mentioning the member
   channel.send(`Welcome to the server, ${member}`);
   
 })
 
-,client.login(Give the token here))
+,client.login(token)
